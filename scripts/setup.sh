cd6dfa2bf4ed500dc46434b299dd1c4184d2ac3d
Yagiz Kanbur <150202055@kocaeli.edu.tr>
2021-08-02 19:05:24 +0300

    scripts changed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,12 +9,12 @@ else
 fi
 
 docker exec -t $container couchbase-cli cluster-init --cluster-name basket-microservice-cluster \
-   --cluster-username Administrator --cluster-password opensesame --services data,index,query,fts,eventing,analytics \
+   --cluster-username Administrator --cluster-password password --services data,index,query,fts,eventing,analytics \
    --cluster-ramsize 2048
 
-docker exec -t $container couchbase-cli bucket-create -c localhost -u Administrator -p opensesame \
+docker exec -t $container couchbase-cli bucket-create -c localhost -u Administrator -p password \
   --bucket basket --bucket-type couchbase --bucket-ramsize 1024
 
-docker exec -t $container couchbase-cli user-manage -c localhost -u Administrator -p opensesame \
+docker exec -t $container couchbase-cli user-manage -c localhost -u Administrator -p password \
   --set --rbac-username yagiz --rbac-name yagiz --rbac-password opensesame --roles admin \
   --auth-domain local
