a1c4ba9747d41711be418edd95c10c18ebefa8f9
Yagiz Kanbur <150202055@kocaeli.edu.tr>
2021-08-02 18:28:30 +0300

    scripts added

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,13 +8,13 @@ else
   echo 'Container captured=' $container
 fi
 
-docker exec -t $container couchbase-cli cluster-init --cluster-name user-follows-product-cluster \
-   --cluster-username Administrator --cluster-password password --services data,index,query,fts,eventing,analytics \
+docker exec -t $container couchbase-cli cluster-init --cluster-name basket-microservice-cluster \
+   --cluster-username Administrator --cluster-password opensesame --services data,index,query,fts,eventing,analytics \
    --cluster-ramsize 2048
 
-docker exec -t $container couchbase-cli bucket-create -c localhost -u Administrator -p password \
-  --bucket followed_products --bucket-type couchbase --bucket-ramsize 1024
+docker exec -t $container couchbase-cli bucket-create -c localhost -u Administrator -p opensesame \
+  --bucket basket --bucket-type couchbase --bucket-ramsize 1024
 
-docker exec -t $container couchbase-cli user-manage -c localhost -u Administrator -p password \
-  --set --rbac-username myapp --rbac-name myapp --rbac-password 123321 --roles admin \
+docker exec -t $container couchbase-cli user-manage -c localhost -u Administrator -p opensesame \
+  --set --rbac-username yagiz --rbac-name yagiz --rbac-password opensesame --roles admin \
   --auth-domain local
